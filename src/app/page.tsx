d60a0ef22f2a80b36e3c04941619c5e656182fab
vatsal prajapati <prajapativatsal555@gmail.com>
2025-04-22 19:10:38 +0530

    feat(home): add new sections to the home page
    Introduce DemoSection, HowItWorkSection, PricingSection, and CTASection to enhance the home page's content and user engagement.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import BgGradient from "@/components/common/bg-gradient";
+import CTASection from "@/components/home/cta-section";
+import DemoSection from "@/components/home/demo-section";
 import HeroSection from "@/components/home/hero-section";
+import HowItWorkSection from "@/components/home/how-it-work-section";
+import PricingSection from "@/components/home/pricing-section";
 
 const Home = () => {
   return (
@@ -7,6 +11,10 @@ const Home = () => {
       <BgGradient />
       <div className="flex flex-col">
         <HeroSection />
+        <DemoSection />
+        <HowItWorkSection />
+        <PricingSection />
+        <CTASection />
       </div>
     </div>
   );
