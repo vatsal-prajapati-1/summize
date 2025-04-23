1a260e50db0e0b1d7e5e6b33d59d45e3bdb7f7e9
vatsal prajapati <prajapativatsal555@gmail.com>
2025-04-23 18:57:07 +0530

    feat: add upload page component for logged-in users
    This commit introduces a new page component for the upload feature, which is accessible only to logged-in users. The component includes a header, a gradient background, and a form for uploading content.

---
+++ src/app/(logged-in)/upload/page.tsx
@@ -0,0 +1,19 @@
+import UploadHeader from "@/components/upload/upload-header";
+import BgGradient from "@/components/common/bg-gradient";
+import UploadForm from "@/components/upload/upload-form";
+
+const page = () => {
+  return (
+    <section className="min-h-screen">
+      <BgGradient />
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <UploadHeader />
+          <UploadForm />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default page;
