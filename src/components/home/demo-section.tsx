8f3e5f9d130b81dbb437bf26dbad71c66a5f44b9
vatsal prajapati <prajapativatsal555@gmail.com>
2025-04-23 18:59:50 +0530

    style(home): remove trailing space in demo-section className
    This commit removes an unnecessary trailing space in the className of the demo-section component to maintain code cleanliness and consistency.

--- src/components/home/demo-section.tsx
+++ src/components/home/demo-section.tsx
@@ -3,7 +3,7 @@ import { Pizza } from "lucide-react";
 const DemoSection = () => {
   return (
     <section className="relative">
-      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12 ">
+      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
         <div
           aria-hidden="true"
           className="pointer-events-none absolute inset-0 -z-10 transform-gpu overflow-hidden blur-3xl"
