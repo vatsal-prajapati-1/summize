fbb6744c3ff3a5d9887e8ecd0ccdb953e5330a1c
vatsal prajapati <prajapativatsal555@gmail.com>
2025-04-23 19:06:11 +0530

    feat(upload): add upload form component for PDF files
    This commit introduces a new component `upload-form.tsx` that handles the upload and validation of PDF files. The component includes file size and type validation, upload progress feedback, and error handling using toast notifications. The form utilizes `useUploadThing` for file uploads and `generatePdfSummary` for processing the uploaded PDF.

---
+++ src/components/upload/upload-form.tsx
@@ -0,0 +1,120 @@
+"use client";
+import generatePdfSummary from "@/actions/upload-actions";
+import UploadFormInput from "@/components/upload/upload-form-input";
+import { useUploadThing } from "@/utils/uploadthing";
+import { useRef, useState } from "react";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid file" })
+    .refine(
+      (file) => file.size < 20 * 1024 * 1024,
+      "File size must be less than 20mb"
+    )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a PDF"
+    ),
+});
+
+const UploadForm = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { startUpload, routeConfig } = useUploadThing("pdfUploader", {
+    onClientUploadComplete: () => {
+      console.log("uploaded successfully!");
+    },
+    onUploadError: (err) => {
+      console.log("error occurred while uploading", err);
+      toast.error("Error occurred while uploading", {
+        description: err.message,
+      });
+    },
+    onUploadBegin: (file: string) => {
+      console.log("upload has begun for", file);
+    },
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      console.log("Submitted");
+
+      const formData = new FormData(e.currentTarget);
+
+      const file = formData.get("file") as File;
+
+      const validateFields = schema.safeParse({ file });
+
+      console.log(validateFields, "validate fields should be console logged");
+
+      if (!validateFields.success) {
+        //   console.log(
+        //     validateFields.error.flatten().fieldErrors.file?.[0] ?? "Invalid file"
+        //   );
+
+        toast.error("❌ Something went wrong", {
+          description:
+            validateFields.error.flatten().fieldErrors.file?.[0] ??
+            "Invalid file",
+        });
+
+        setIsLoading(false);
+
+        return;
+      }
+
+      toast.message("📄Uploading PDF...", {
+        description: "We are uploading your PDF! ",
+      });
+
+      const response = await startUpload([file]);
+
+      if (!response) {
+        toast.error("Something went wrong", {
+          description: "Please use a different file",
+        });
+        setIsLoading(false);
+        return;
+      }
+
+      toast.message("📄Processing PDF", {
+        description: "Hang tight! Our AI is reading through your document! ✨",
+      });
+
+      const result = await generatePdfSummary(response);
+
+      const { data = null, message = null } = result || {};
+
+      if (data) {
+        toast.message("📄 Saving PDF...", {
+          description: "Hang tight! We are saving your summary! ✨",
+        });
+        formRef.current?.reset();
+        if(data.summary) {
+          
+        }
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.error("Error occurred", error);
+      formRef.current?.reset();
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
+      <UploadFormInput
+        isLoading={isLoading}
+        ref={formRef}
+        onSubmit={handleSubmit}
+      />
+    </div>
+  );
+};
+
+export default UploadForm;
