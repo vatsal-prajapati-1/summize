9140bf266c3fa1705e53be23c8ad81ecf4593a22
vatsal-prajapati-1 <prajapativatsal555@gmail.com>
2025-04-29 18:06:20 +0530

    fix bugs in next config files

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["http://localhost:3000", "https://*.loca.lt"],
-  experimental: {
-    serverComponentsExternalPackages: ["..."],
-  },
-  maxDuration: 60,
   /* config options here */
 };
 
